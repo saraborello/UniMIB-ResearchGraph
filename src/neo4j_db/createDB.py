972ebaa4285f5d59a930fd8a7fd42ac5e64b5e78
saraborello <saraaborello@gmail.com>
2024-11-21 22:49:46 +0100

    Db creation, fixing problem with authors

--- src/neo4j_db/createDB.py
+++ src/neo4j_db/createDB.py
@@ -19,29 +19,44 @@ class Neo4jConnector:
 
 # Nuova Query Cypher
 query = """
-// Caricamento dei dati da authors.csv
+// Caricamento dei dati da Authors_internal.csv
 LOAD CSV WITH HEADERS FROM 'file:///Authors_internal.csv' AS author_row
-WITH author_row WHERE author_row.`ORCID ID` IS NOT NULL AND author_row.`ORCID ID` <> ""
+WITH author_row 
+WHERE TRIM(author_row.`ORCID ID`) IS NOT NULL AND TRIM(author_row.`ORCID ID`) <> ""
 MERGE (a:Author {orcid: TRIM(author_row.`ORCID ID`)})
 SET a.nome = author_row.`Given Name`,
     a.cognome = author_row.`Family Name`,
-    a.keywords = split(author_row.Keywords, ", ")
-WITH 1 AS dummy // Mantiene il contesto per il prossimo blocco
+    a.keywords = split(author_row.Keywords, ", "),
+    a.department_code = author_row.`Department Code`,
+    a.specific_field = author_row.`Specific Field`,
+    a.role = author_row.Role,
+    a.organization = author_row.Organization,
+    a.h_index = toInteger(author_row.`H Index`),
+    a.citations = toInteger(author_row.Citations),
+    a.past_institutions = split(author_row.`Past Institutions`, ", ")
+WITH 1 AS dummy // Mantieni il contesto per passare al blocco successivo
 
-// Caricamento dei dati da papers.csv
+// Caricamento dei dati da papers.csv e creazione delle relazioni
 LOAD CSV WITH HEADERS FROM 'file:///papers.csv' AS paper_row
-WITH paper_row, split(TRIM(paper_row.Authors), ",") AS author_orcids
+WITH paper_row, 
+     [orcid IN split(paper_row.Authors, ",") | TRIM(orcid)] AS author_orcids
+WHERE TRIM(paper_row.Doi) IS NOT NULL AND TRIM(paper_row.Doi) <> ""
 MERGE (p:Paper {doi: TRIM(paper_row.Doi)})
 SET p.title = paper_row.Title,
     p.topic = paper_row.Topic,
+    p.subfield = paper_row.Subfield,
+    p.field = paper_row.Field,
+    p.domain = paper_row.Domain,
+    p.cites = toInteger(paper_row.Cites),
+    p.cited_by = toInteger(paper_row.Cited_by),
+    p.keywords = split(paper_row.Keywords, ", "),
+    p.year = toInteger(paper_row.Year),
     p.url = paper_row.Url
-WITH p, author_orcids // Passa il contesto ai prossimi blocchi
+WITH p, author_orcids
+UNWIND author_orcids AS orcidId
+MATCH (a:Author {orcid: orcidId}) // Connettiti solo agli autori esistenti
+MERGE (a)-[:HAS_WRITTEN]->(p)
 
-// Creazione delle relazioni tra Paper e Author
-FOREACH (orcidId IN author_orcids |
-    MERGE (a:Author {orcid: TRIM(orcidId)})
-    MERGE (a)-[:HAS_WRITTEN]->(p)
-)
 
 """
 
