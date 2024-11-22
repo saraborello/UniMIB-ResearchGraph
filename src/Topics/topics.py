e34882d5a3e64e07ca622c1fbc2f142488a6db87
keitaVigano <keitainter@gmail.com>
2024-11-22 16:36:27 +0100

    Retrieving correct topics

--- src/Topics/topics.py
+++ src/Topics/topics.py
@@ -10,22 +10,21 @@ def get_topics(data) -> list:
     topic_dict = {}
 
     for index, row in data.iterrows():
-        topic = row["Topic"]
         subfield = row["Subfield"]
         field = row["Field"]
         domain = row["Domain"]
-        if topic not in topic_dict:
-            topic_dict[topic] = [field, subfield, domain]
+        if subfield not in topic_dict:
+            topic_dict[subfield] = [field, domain]
 
     return list(topic_dict.items())
 
 
-def list_to_csv(topic_list):
+def list_to_csv(topic_list) -> None:
     with open('data/processed/topic.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(['Topic', 'Field', 'Subfield', 'Domain'])
+        writer.writerow(['Subfield', 'Field', 'Domain'])
         for topic in topic_list:
-            writer.writerow([topic[0], topic[1][0],  topic[1][1], topic[1][2]])
+            writer.writerow([topic[0], topic[1][0],  topic[1][1]])
 
 
 if __name__ == "__main__":
